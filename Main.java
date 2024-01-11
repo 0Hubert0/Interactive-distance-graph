32504101bc172f60dd8588618518768158351aab
0Hubert0 <95587852+0Hubert0@users.noreply.github.com>
2024-01-11 14:02:48 +0000

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,11 +3,11 @@ package sample;
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
 import javafx.application.Application;
-import javafx.scene.Cursor;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Button;
+import javafx.scene.input.KeyCode;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
@@ -23,9 +23,11 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         final double[] cursorX = new double[1];
         final double[] cursorY = new double[1];
-        final int[] x = new int[1];
-        final int[] step = {4};
+        final int[] step = {3};
+        final int[] x = {100};
         List<Point> circles = new ArrayList<>();
+        final boolean[] isStopped = {true};
+        x[0]-=step[0];
 
         AnchorPane root = new AnchorPane();
 
@@ -44,44 +46,74 @@ public class Main extends Application {
 
         GraphicsContext gr = canvas.getGraphicsContext2D();
         gr.beginPath();
+        gr.moveTo(x[0], 300);
+        gr.lineTo(canvas.getWidth()-100, 300);
+        gr.moveTo(x[0], 300);
+        gr.lineTo(x[0], 100);
+        gr.stroke();
+        gr.setLineWidth(2);
 
         Timeline timeline = new Timeline(new KeyFrame(Duration.millis(50), event -> {
             for (Point p : circles) {
-                p.getDistances().add(Math.sqrt(Math.pow(cursorX[0]-p.getCenterX(), 2)+Math.pow(cursorY[0]-p.getCenterY(), 2)));
+                if(p.getCurrentDistance()!=-1)p.setPreviousDistance(p.getCurrentDistance()/5);
+                p.setCurrentDistance(Math.sqrt(Math.pow(cursorX[0]-p.getCenterX(), 2)+Math.pow(cursorY[0]-p.getCenterY(), 2)));
             }
-            x[0] =100;
             background.setWidth(scene.getWidth());
             background.setHeight(scene.getHeight());
-            gr.clearRect(0, 0, canvas.getWidth(), canvas.getHeight());
-            gr.moveTo(x[0], 300);
-            gr.lineTo(canvas.getWidth()-100, 300);
-            gr.moveTo(x[0], 300);
-            gr.lineTo(x[0], 100);
-            gr.stroke();
-            gr.moveTo(x[0], 300);
-            for (int i = 0; i <circles.get(0).getDistances().size(); i++) {
-                x[0]+= step[0];
-                gr.lineTo(x[0], 300-circles.get(0).getDistances().get(i)/10);
+            x[0]+=step[0];
+            for (Point p : circles) {
+                gr.beginPath();
+                gr.setStroke(p.getFill());
+                if(p.getPreviousDistance()!=-1) gr.moveTo(x[0] - step[0], 300 - p.getPreviousDistance());
+                else gr.moveTo(x[0] - step[0], 300 - p.getCurrentDistance()/5);
+                gr.lineTo(x[0], 300 - p.getCurrentDistance() / 5);
+                gr.stroke();
             }
-            gr.stroke();
-            if(x[0]>canvas.getWidth()) step[0]--;
         }));
+
+        timeline.setCycleCount((int)((canvas.getWidth()-100-x[0]))/step[0]);
+
+        scene.setOnKeyPressed(event -> {
+            if(event.getCode()==KeyCode.SPACE && circles.size()>0){
+                isStopped[0]=!isStopped[0];
+                if ((isStopped[0])) {
+                    timeline.pause();
+                } else {
+                    timeline.play();
+                }
+            }
+        });
 
         Button add = new Button("add point");
         add.setLayoutY(scene.getHeight()-50);
         add.setLayoutX(300);
         add.setOnAction(event -> {
             Point c = new Point(Math.random()*(scene.getWidth()-50)+50,
-                    Math.random()*(scene.getHeight()-canvas.getHeight()-50)+50, 2);
+                    Math.random()*(scene.getHeight()-canvas.getHeight()-50)+50, 4);
             c.setFill(Color.rgb((int)(Math.random()*127), (int)(Math.random()*127), (int)(Math.random()*127)));
             circles.add(c);
             root.getChildren().add(c);
-            if(circles.size()==1)timeline.play();
         });
 
-        root.getChildren().addAll(background, canvas, add);
+        Button play = new Button("Play");
+        play.setLayoutY(scene.getHeight()-50);
+        play.setLayoutX(500);
+        play.setOnAction(event -> {
+            if(circles.size()>0) {
+            timeline.play();
+            isStopped[0]=false;
+            }
+        });
 
-        timeline.setCycleCount(-1);
+        Button pause = new Button("Pause");
+        pause.setLayoutY(scene.getHeight()-50);
+        pause.setLayoutX(700);
+        pause.setOnAction(event -> {
+            timeline.pause();
+            isStopped[0] =true;
+        });
+
+        root.getChildren().addAll(background, canvas, add, play, pause);
 
         Stage stage = new Stage();
         stage.setScene(scene);
