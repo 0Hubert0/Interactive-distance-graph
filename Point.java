0695354d7c6864932f295dd4b5946849b2fa8cc3
0Hubert0 <95587852+0Hubert0@users.noreply.github.com>
2024-01-11 14:03:17 +0000

    Update Point.java

--- Point.java
+++ Point.java
@@ -1,16 +1,32 @@
 package sample;
 
-import javafx.scene.paint.Paint;
 import javafx.scene.shape.Circle;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class Point extends Circle {
+    private double currentDistance=-1, previousDistance=-1;
     private List<Double> distances = new ArrayList<>();
 
     public Point(double centerX, double centerY, double radius) {
         super(centerX, centerY, radius);
+    }
+
+    public double getCurrentDistance() {
+        return currentDistance;
+    }
+
+    public void setCurrentDistance(double currentDistance) {
+        this.currentDistance = currentDistance;
+    }
+
+    public double getPreviousDistance() {
+        return previousDistance;
+    }
+
+    public void setPreviousDistance(double previousDistance) {
+        this.previousDistance = previousDistance;
     }
 
     public List<Double> getDistances() {
